1eb768123aa949ce8653d66d06e24d13e4eaa190
ndtsman <alanliuyc@gmail.com>
2023-07-12 23:32:26 +1000

    Display the rolling dice

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ score1El.textContent = 0;
 
 const diceEl = document.querySelector('.dice');
 diceEl.classList.add('hidden');
+
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+
+btnRoll.addEventListener('click', function () {
+  // Move down the variable here to be called when ever the btn is clicked.
+  let dice = Math.trunc(Math.random() * 6) + 1;
+
+  //display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
